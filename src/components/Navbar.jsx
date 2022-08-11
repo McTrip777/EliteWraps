c6de6dd358342f2fd5425297ccf73f823486e39a
Jake McFaul <jacob.m.mcfaul@gmail.com>
2022-08-11 14:20:11 -0600

    home page
    added styles and image to home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../images/Elite_Wraps_Logo.png'
 import '../styles/navbar.css'
 
+
+
 function Navbar() {
+    const [nav, setNav] = useState("navbar")
+
+    const listenScrollEvent = (event) => {
+    if (window.scrollY < 140) {
+        return setNav("navbar")
+    } else if (window.scrollY > 138) {
+        return setNav("navbar2")
+    } 
+    }
+
+    useEffect(() => {
+    window.addEventListener('scroll', listenScrollEvent);
+
+    return () =>
+        window.removeEventListener('scroll', listenScrollEvent);
+    }, []);
+
     return (
-        <div className="navbar">
+        <div className={nav}>
             <Link className="homeLogo" to="/">
                 <img src={logo} alt="Elite Wraps Logo"/>
             </Link>
