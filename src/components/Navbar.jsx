eccfb61aac515f1ccb8f337e3a15cbc1e03705c8
Jake McFaul <jacob.m.mcfaul@gmail.com>
2022-08-23 15:43:39 -0600

    Revert "Updates"
    This reverts commit d9c5860b31689cd4183b93028eecd2c785cd5587.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../images/Elite_Wraps_Logo.png'
+import '../styles/navbar.css'
+
+
+
+function Navbar() {
+    const [nav, setNav] = useState("navbar")
+
+    const listenScrollEvent = (event) => {
+    if (window.scrollY < 140) {
+        return setNav("navbar")
+    } else if (window.scrollY > 138) {
+        return setNav("navbar2")
+    } 
+    }
+
+    useEffect(() => {
+    window.addEventListener('scroll', listenScrollEvent);
+
+    return () =>
+        window.removeEventListener('scroll', listenScrollEvent);
+    }, []);
+
+    return (
+        <div className={nav}>
+            <Link className="homeLogo" to="/">
+                <img src={logo} alt="Elite Wraps Logo"/>
+            </Link>
+            <div className="navbarRoutes">
+                <Link to="/">Home</Link>
+                <Link to="/gallery">Gallery</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
